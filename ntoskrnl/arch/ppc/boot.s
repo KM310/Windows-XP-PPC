368522f7a1d98f2ba9958ccb2c753ea5d3816223
K_M <julius@marcel-loos.de>
2026-01-20 21:30:36 +0100

    Create boot.s

---
+++ ntoskrnl/arch/ppc/boot.s
@@ -0,0 +1,15 @@
+# =====================================================
+#  ntoskrnl entry point
+#  Copyright (c) 2026 K_M310
+#  Independent reimplementation – no Microsoft code
+# =====================================================
+
+.section .text
+.global _start
+
+_start:
+lis r1, kernel_stack_top@h
+ori r1, r1, kernel_stack_top@1
+
+bl NtKernelEntry
+1: b 1b
