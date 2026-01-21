13a7616078b435c411084cf2be73a092035fe78c
K_M <julius@marcel-loos.de>
2026-01-21 15:44:49 +0100

    Create context_switch.s

---
+++ ntoskrnl/arch/ppc/context_switch.s
@@ -0,0 +1,11 @@
+# =====================================================
+#  context_switch.s – PPC file
+#  Copyright (c) 2026 K_M310
+#  Independent reimplementation – no Microsoft code
+# =====================================================
+
+.global KeContextSwitch
+
+KeContextSwitch:
+    # Stub
+    blr 
