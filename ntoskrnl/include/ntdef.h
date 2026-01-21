9319816f251e03df26d8ec17ee94684ef2ff9d69
K_M <julius@marcel-loos.de>
2026-01-21 16:45:37 +0100

    Create ntdef.h

---
+++ ntoskrnl/include/ntdef.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef unsigned char  UCHAR;
+typedef unsigned short USHORT;
+typedef unsigned long  ULONG;
+typedef signed long    NTSTATUS;
+typedef void*          PVOID;
+
+#define STATUS_SUCCESS 0x00000000
+#define STATUS_UNSUCCESSFUL 0xC0000001
+
+#define TRUE  1
+#define FALSE 0
