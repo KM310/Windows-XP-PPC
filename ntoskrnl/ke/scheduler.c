1e89fd2b0d159220c2df7f1929ff1e418071bddf
K_M <julius@marcel-loos.de>
2026-01-21 16:00:09 +0100

    Create scheduler.c

---
+++ ntoskrnl/ke/scheduler.c
@@ -0,0 +1,17 @@
+//Copyright Julius (KM310)
+//written by Julius
+void KeSchedule(void) {
+    ETHREAD* next = KeSelectNextThread();
+    if (!next)
+        return; // no Thread ready
+
+    ETHREAD* old = CurrentThread;
+    CurrentThread = next;
+
+    // reuse old Thread
+    if (old)
+        KeReadyThread(old);
+
+    // Context-Switch
+    KeContextSwitch(old, next);
+}
