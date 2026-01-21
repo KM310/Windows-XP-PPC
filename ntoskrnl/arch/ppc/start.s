d01e913846f953cde796e067d5f0ae07dcd783f7
K_M <julius@marcel-loos.de>
2026-01-21 06:34:04 +0100

    Update start.s

--- ntoskrnl/arch/ppc/start.s
+++ ntoskrnl/arch/ppc/start.s
@@ -8,8 +8,11 @@
 .global _start
 
 _start:
-lis r1, kernel_stack_top@h
-ori r1, r1, kernel_stack_top@1
+    lis r1, kernel_stack_top@h
+    ori r1, r1, kernel_stack_top@l
+    bl NtKernelEntry
+1:  b 1b
 
-bl NtKernelEntry
-1: b 1b
+.section .bss
+.space 8192
+kernel_stack_top:
