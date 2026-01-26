2dd84a8a01e60aa01b191e3da866ed77ce4810a4
K_M <julius@marcel-loos.de>
2026-01-26 15:37:40 +0000

    Add files via upload

---
+++ wxposkrnl/mm/mm.c
@@ -0,0 +1,11 @@
+//Copyright Julius
+//written by Julius
+#include "mm.h"
+
+void MmInitialize(void) {
+    /* Kernel Heap + Page Layout */
+}
+
+PVOID MmAllocateKernelMemory(ULONG Size) {
+    return 0;
+}
