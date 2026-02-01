cc61ce3ee2219e122ee7505b5d7828dc006564d1
K_M <julius@marcel-loos.de>
2026-02-01 20:48:42 +0100

    Change UART_BASE address for QEMU 40p
    Updated UART_BASE address for QEMU 40p compatibility.

--- wxposkrnl/drivers/uart.c
+++ wxposkrnl/drivers/uart.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-#define UART_BASE 0x10000000
+#define UART_BASE 0x800003F8   // Right UART for QEMU 40p
 
 void uart_print(const char* s) {
     volatile uint8_t* uart = (uint8_t*)UART_BASE;
