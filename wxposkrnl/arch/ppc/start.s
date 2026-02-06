b76baaed166cad5e88c4138d43d2129812aebf3c
K_M <julius@marcel-loos.de>
2026-02-06 23:48:16 +0100

    Improve stack initialization in start.s
    Replaced stack initialization with a better instruction for clarity.

--- wxposkrnl/arch/ppc/start.s
+++ wxposkrnl/arch/ppc/start.s
@@ -4,12 +4,11 @@
 #  Independent reimplementation – no Microsoft code
 # =====================================================
 
-    .globl _start
-    .type _start, @function
+.globl _start
+.type _start, @function
 
 _start:
-    lis     r1, stack_top@ha
-    addi    r1, r1, stack_top@l
+    la      r1, stack_top      # better r1
 
     bl      NtKernelEntry
 
